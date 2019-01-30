5443cfefb7ddab14b6629a8281153e690dfc1c42
Bruce Harms <bruceh_88@comcast.net>
2019-01-29 20:42:03 -0500

    fixed redirect uri

--- client/src/utils/Auth/Auth.js
+++ client/src/utils/Auth/Auth.js
@@ -4,7 +4,7 @@ export default class Auth {
   auth0 = new auth0.WebAuth({
     domain: 'esengpta.auth0.com',
     clientID: 'Apb7iVrs0T3bTnOmWeKpHUbaaiRm7DS0',
-    redirectUri: 'http://localhost:3000/',
+    redirectUri: 'https://worldcuisinerecipe.herokuapp.com/',
     responseType: 'token id_token',
     scope: 'openid profile email'
   });
